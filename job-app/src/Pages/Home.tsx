496b0a83adbb24572002f0eb33ab5d2b89670f97
Anmoljagota <anmoljagota08@gmail.com>
2024-03-20 18:03:01 +0530

    frontend structure made and backend structure added

---
+++ job-app/src/Pages/Home.tsx
@@ -0,0 +1,130 @@
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+
+import {
+  AddIcon,
+  EditIcon,
+  ExternalLinkIcon,
+  HamburgerIcon,
+  RepeatIcon,
+} from "@chakra-ui/icons";
+import JobCategory from "../components/JobCategory";
+import { InitialFocus } from "../components/JoPostModal";
+const Home = () => {
+  const isSmallScreen = useBreakpointValue({ base: true, md: false });
+  return (
+    <div>
+      <Box className="bg-[#211CA6]">
+        <Box className="p-4 base:p-0 md:p-6 sm:p-4 w-11/12 md:w-11/12 xsm:w-12/12 base:w-12/12  m-auto">
+          <nav className="flex justify-between items-center">
+            {isSmallScreen && (
+              <Menu>
+                <MenuButton
+                  as={IconButton}
+                  aria-label="Options"
+                  icon={<HamburgerIcon />}
+                  variant="outline"
+                  color={"white"}
+                  fontWeight={"700"}
+                />
+                <MenuList>
+                  <MenuItem icon={<AddIcon />} command="⌘T">
+                    New Tab
+                  </MenuItem>
+                  <MenuItem icon={<ExternalLinkIcon />} command="⌘N">
+                    New Window
+                  </MenuItem>
+                  <MenuItem icon={<RepeatIcon />} command="⌘⇧N">
+                    Open Closed Tab
+                  </MenuItem>
+                  <MenuItem icon={<EditIcon />} command="⌘O">
+                    Open File...
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            )}
+            {/* Website Name */}
+            <div className="text-white text-[2.5rem] font-bold text-center">
+              BreakDoor.com
+            </div>
+            {/* Navigation Links */}
+            {!isSmallScreen && (
+              <ul className="flex justify-center space-x-4 gap-10 text-[1.5rem]">
+                <li className="text-white hover:text-gray-300 cursor-pointer">
+                  Home
+                </li>
+                <li className="text-white hover:text-gray-300 cursor-pointer">
+                  Company
+                </li>
+                <li className="text-white hover:text-gray-300 cursor-pointer">
+                  Company Jobs
+                </li>
+                <li className="text-white hover:text-gray-300 cursor-pointer">
+                  Facts
+                </li>
+              </ul>
+            )}
+            {/* Find Jobs Button */}
+            <InitialFocus />
+          </nav>
+          <Center display={"flex"} flexDirection={"column"}>
+            <Box
+              fontSize={"5rem"}
+              textAlign={"center"}
+              color={"#fff"}
+              fontWeight={"700"}
+              w={"35%"}
+              display={"flex"}
+              flexDirection={"column"}
+              gap={"4rem"}
+            >
+              <Box>
+                <Text fontWeight={"500"} fontSize={"1.5rem"} mt={"8rem"}>
+                  We have 1M+ great job offers you deserve!
+                </Text>
+                <Text lineHeight={"6rem"}>Largest Job Site In The World</Text>
+              </Box>
+              <Flex
+                fontSize={"2rem"}
+                gap={"1rem"}
+                justifyContent={"flex-start"}
+              >
+                {/* First Button */}
+                <Button color="white" p={"1.8rem 3rem"} textColor="black">
+                  FIND A JOB
+                </Button>
+
+                {/* Second Button */}
+                <Button
+                  bg="#03A63C"
+                  color="white"
+                  size={"lg"}
+                  p={"1.8rem 4rem"}
+                >
+                  FIND A CANDIDATE
+                </Button>
+              </Flex>
+            </Box>
+          </Center>
+        </Box>
+      </Box>
+      {/* JOB CATEGORY */}
+      <Box w={"55%"} m={"auto"}>
+        <JobCategory />
+      </Box>
+    </div>
+  );
+};
+
+export default Home;
---
+++ job-app/src/components/JoPostModal.tsx
@@ -0,0 +1,165 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Select,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { MdOutlineAdd } from "react-icons/md";
+import { HStack } from "@chakra-ui/react";
+
+import Skills from "./skills/Skills";
+
+export function InitialFocus() {
+  const [skills, setSkills] = useState<{ name: string }[]>([]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
+  const AddNewSkill = () => {
+    setSkills([...skills, { name: "" }]);
+  };
+  const onChange = (value: string) => {
+    console.log("i am value", value);
+  };
+  return (
+    <>
+      <Button
+        background={"#F27405"}
+        variant="solid"
+        fontSize="1.3rem"
+        color={"#fff"}
+        p={{ md: "1.8rem 4.5rem", base: "1.5rem 2rem" }}
+        _hover={{ background: "#EE9342" }}
+        borderRadius={"20px"}
+        onClick={onOpen}
+      >
+        Post Jobs
+      </Button>
+
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent maxW={"80vw"} w={{ sm: "80vw", lg: "50vw" }}>
+          <ModalHeader fontSize={"2rem"}>Job Details</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody
+            pb={6}
+            width={"90%"}
+            m={"auto"}
+            display={"flex"}
+            flexDirection={"column"}
+            gap={"1rem"}
+          >
+            <FormControl>
+              <FormLabel fontSize={"1.2rem"}>Job Title</FormLabel>
+              <Input ref={initialRef} placeholder="First name" p={"1.5rem"} />
+            </FormControl>
+            <Flex gap={"1rem"}>
+              <FormControl>
+                <FormLabel fontSize={"1.2rem"}>Department</FormLabel>
+                <Select placeholder="department" size="lg" />
+              </FormControl>
+              <FormControl>
+                <FormLabel fontSize={"1.2rem"}>Job Type</FormLabel>
+                <Select placeholder="job type" size="lg" />
+              </FormControl>
+            </Flex>
+            <Flex gap={"1rem"}>
+              <FormControl>
+                <FormLabel fontSize={"1.2rem"}>Recruitment Quota</FormLabel>
+                <Input placeholder={"100"} p={"1.5rem"} />
+              </FormControl>
+              <FormControl>
+                <FormLabel fontSize={"1.2rem"}>Recruitment Period</FormLabel>
+                <input
+                  type="date"
+                  placeholder="Select date"
+                  className="p-[0.5rem] w-[100%]"
+                  style={{ border: "0.5px solid #EAE7E7" }}
+                />
+              </FormControl>
+            </Flex>
+            <Flex gap={"1rem"}>
+              <FormControl>
+                <FormLabel fontSize={"1.2rem"}>Expected Salary</FormLabel>
+                <Input placeholder={"100"} p={"1.5rem"} />
+              </FormControl>
+              <FormControl>
+                <FormLabel fontSize={"1.2rem"}>Experience in Years</FormLabel>
+                <input
+                  type="date"
+                  placeholder="Select date"
+                  className="p-[0.5rem] w-[100%]"
+                  style={{ border: "0.5px solid #EAE7E7" }}
+                />
+              </FormControl>
+            </Flex>
+            <Flex gap={"1rem"}>
+              <FormControl>
+                <FormLabel fontSize={"1.2rem"}>Location</FormLabel>
+                <Input placeholder={"location"} p={"1.5rem"} />
+              </FormControl>
+              <FormControl>
+                <FormLabel fontSize={"1.2rem"}>
+                  Hiring Managers (Optional)
+                </FormLabel>
+                <input
+                  placeholder="Select Manager"
+                  className="p-[0.7rem] w-[100%]"
+                  style={{ border: "0.5px solid #EAE7E7" }}
+                />
+              </FormControl>
+            </Flex>
+            <FormControl>
+              <FormLabel fontSize={"1.2rem"}>Skill Sets</FormLabel>
+              <HStack onClick={AddNewSkill} cursor={"pointer"}>
+                <Box
+                  background={"#03A63C"}
+                  className="xs:h-[42px] xs:w-[42px] h-8 w-[20px] rounded-full flex justify-center items-center cursor-pointer"
+                >
+                  <MdOutlineAdd
+                    className="xs:h-6 xs:w-6 h-4 w-4"
+                    //   style={{ background: "#03A63C" }}
+                    color="#fff"
+                  />
+                </Box>
+                <Text color={"#03A63C"} fontSize={"1.2rem"} fontWeight={"bold"}>
+                  Add New Skill
+                </Text>
+              </HStack>
+            </FormControl>
+            <Flex flexWrap={"wrap"} gap={"1rem"}>
+              {skills.map((ele) => (
+                <Skills key={ele.name} onChange={onChange} />
+              ))}
+            </Flex>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
